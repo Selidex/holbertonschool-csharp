6a6598c7e4ef52f32d5d762875bc0d9a0d5b2808
Selidex <justindparnell@yahoo.com>
2021-07-12 11:27:51 -0500

    Uploading task 9 files

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:0.##}%\nCurrency: ${1:0.##}", percent*100, currency);
+	}
+}
