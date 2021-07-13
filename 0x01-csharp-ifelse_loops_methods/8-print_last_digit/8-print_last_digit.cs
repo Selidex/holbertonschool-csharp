e7595e8d417e0041653b3a96a9466eee55de2cfa
Selidex <justindparnell@yahoo.com>
2021-07-13 18:15:25 -0500

    Uploading task 8 files

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    class Number{
+        public static int PrintLastDigit(int number)
+        {
+            int last = number % 10;
+            if(last < 0)
+            {
+                Console.Write("{0}", last * -1);
+                return last * -1;
+            }
+            else
+            {
+                Console.Write("{0}", last);
+                return last;
+            }
+        }
+    }
