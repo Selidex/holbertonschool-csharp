620b7f15ad30f2ab9e71460cd3769b506d43b13c
Selidex <justindparnell@yahoo.com>
2021-07-14 16:11:25 -0500

    Uploading task 5 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List{
+    public static int MaxInteger(List<int> myList){
+        if(myList == null || myList.Count == 0)
+            return -1;
+        int max = myList[0];
+        for(int i = 1; i < myList.Count; i++)
+        {
+            if(max < myList[i])
+            {
+                max = myList[i];
+            }
+        }
+        return max;
+    }
+}
