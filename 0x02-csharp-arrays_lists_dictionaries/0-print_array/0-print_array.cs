287c8d13379a81874cee55c4c1f6ad1e2fb49a63
Selidex <justindparnell@yahoo.com>
2021-07-14 15:25:49 -0500

    Fixing order of checks and inits

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,12 +2,12 @@
 
 class Array{
     public static int[] CreatePrint(int size){
-        int[] newarray = new int[size];
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
+        int[] newarray = new int[size];
         for(int i = 0; i < size; i++)
         {
             newarray[i] = i;
