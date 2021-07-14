a8e34dd46eae77fbeeef81fa20690a2421b696c6
Selidex <justindparnell@yahoo.com>
2021-07-14 01:00:33 -0500

    Uploading task 0 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array{
+    public static int[] CreatePrint(int size){
+        int[] newarray = new int[size];
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        for(int i = 0; i < size; i++)
+        {
+            newarray[i] = i;
+            Console.Write("{0}", i);
+            if(i != size - 1)
+                Console.Write(" ");
+        }
+        Console.Write("\n");
+        return newarray;
+    }
+}
