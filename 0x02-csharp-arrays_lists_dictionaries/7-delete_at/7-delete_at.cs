2b517587d2cf2712ee705bda50bc0ecca6d7069a
Selidex <justindparnell@yahoo.com>
2021-07-14 17:55:20 -0500

    Uploading task 7 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List{
+    public static List<int> DeleteAt(List<int> myList, int index){
+        if (index < 0 || myList == null || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        for(int i = myList.Count - 1; i >= 0; i--){
+            if(i == index)
+                myList.Remove(i + 1);
+        }
+        return myList;
+    }
+}
