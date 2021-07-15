4abea24307e963f49cd6f0fd468db3c43161e4e9
Selidex <justindparnell@yahoo.com>
2021-07-15 14:27:05 -0500

    Uploading task 5 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static LinkedList<int> CreatePrint(int size){
+        if(size <= 0){
+            Console.Write("\n");
+            return new LinkedList<int>();
+        }
+        LinkedList<int> lList = new LinkedList<int>();
+        for(int i = 0; i < size; i++)
+        {
+            lList.AddLast(i);
+            System.Console.WriteLine(i);
+        }
+        return lList;
+    }
+}
