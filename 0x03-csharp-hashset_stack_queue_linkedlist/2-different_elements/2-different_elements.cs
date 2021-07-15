f0f340d4f0ca8b052bf73b0dd0beb0490373fc74
Selidex <justindparnell@yahoo.com>
2021-07-15 13:45:02 -0500

    Uploading task 2 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2){
+        List<int> newList = new List<int>();
+        foreach(var item in list1){
+            if(!list2.Contains(item) && !newList.Contains(item))
+                newList.Add(item);
+        }
+        foreach(var item in list2){
+                if(!list1.Contains(item) && !newList.Contains(item))
+                    newList.Add(item);
+        }
+        newList.Sort();
+        return newList;
+    }
+}
