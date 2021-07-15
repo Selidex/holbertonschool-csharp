1497eb6529e3bdee1ae6d9822b9bae85d30850ae
Selidex <justindparnell@yahoo.com>
2021-07-15 15:32:03 -0500

    Uploading task 11 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static int Sum(LinkedList<int> myLList){
+        if(myLList.First == null)
+            return 0;
+        int sum = 0;
+        LinkedListNode<int> summer = myLList.First;
+        while(summer != null)
+        {
+            sum += summer.Value;
+            summer = summer.Next;
+        }
+        return sum;
+    }
+}
