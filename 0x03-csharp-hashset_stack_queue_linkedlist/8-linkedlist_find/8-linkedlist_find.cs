a597d37bd67eab6a0172b3fb6e8f5e238fbcbb98
Selidex <justindparnell@yahoo.com>
2021-07-15 15:12:47 -0500

    Uploading task 8 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static int FindNode(LinkedList<int> myLList, int value){
+        if(!myLList.Contains(value))
+            return -1;
+        LinkedListNode<int> check = myLList.First;
+        int i;
+        for(i = 0; check.Value != value; i++)
+            check = check.Next;
+        return i;
+    }
+}
