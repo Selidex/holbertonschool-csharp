9dace8ae9c8494da0804674c40cb3b11d6b7dfd5
Selidex <justindparnell@yahoo.com>
2021-07-14 20:29:11 -0500

    Uploading task 13 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary{
+    public static string BestScore(Dictionary<string, int> myList){
+        if(myList.Count == 0 || myList == null)
+            return "None";
+        int maxv = 0;
+        string maxk = "";
+        foreach(KeyValuePair<string, int> entry in myList)
+        {
+            if(maxv < entry.Value)
+            {
+                maxv = entry.Value;
+                maxk = entry.Key;
+            }
+        }
+        return maxk;
+    }
+}
