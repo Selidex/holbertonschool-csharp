0532bf0971ccd132aa66c1c8ec9ac2cddc452363
Selidex <justindparnell@yahoo.com>
2021-07-15 16:22:37 -0500

    Uploading task 13 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static void Delete(LinkedList<int> myLList, int index){
+        if(index < 0 || index >= myLList.Count)
+            return;
+        LinkedListNode<int> delete = myLList.First;
+        for(int i = 0; i < index; i++)
+            delete = delete.Next;
+        myLList.Remove(delete.Value);
+    }
+}
