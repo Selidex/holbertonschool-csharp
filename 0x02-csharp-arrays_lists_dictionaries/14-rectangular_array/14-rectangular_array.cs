c0eed9becd72d000a9f250664387856f7ace0751
Selidex <justindparnell@yahoo.com>
2021-07-14 20:39:53 -0500

    Uploading task 14 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newArray = new int [5,5];
+            newArray[2,2] = 1;
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    System.Console.Write("{0}", newArray[i,j]);
+                    if(j == 4)
+                        Console.Write("\n");
+                    else
+                        Console.Write(" ");
+                }
+            }
+        }
+    }
+}
