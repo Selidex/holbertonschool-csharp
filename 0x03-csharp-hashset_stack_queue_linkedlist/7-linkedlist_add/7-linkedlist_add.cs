d5f8d20af4729a5e55373585dabbec086a8b56ee
Selidex <justindparnell@yahoo.com>
2021-07-15 14:45:42 -0500

    Uploading task 7 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class LList{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n){
+        myLList.AddFirst(n);
+        return myLList.First;
+    }
+}
