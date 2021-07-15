8e29b35d81abac8eb5dd754cc44cdd879ad7e565
Selidex <justindparnell@yahoo.com>
2021-07-15 03:19:20 -0500

    Uploading task 0 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List{
+    public static int Sum(List<int> myList){
+        List<int> unisum = myList.Distinct().ToList();
+        int sum = 0;
+        for(int i = 0; i < unisum.Count; i++){
+            sum += unisum[i];
+        }
+        return sum;
+    }
+}
