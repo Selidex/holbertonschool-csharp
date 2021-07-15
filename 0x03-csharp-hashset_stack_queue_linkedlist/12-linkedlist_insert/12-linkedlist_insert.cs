f8c7d18010eed6997dd979259745ed6ac406b24b
Selidex <justindparnell@yahoo.com>
2021-07-15 15:43:32 -0500

    Uploading task 12 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n){
+        LinkedListNode<int> insert = myLList.First;
+        if(myLList.First == null || n < myLList.First.Value)
+        {
+            myLList.AddFirst(n);
+            return myLList.First;
+        }
+        while(insert != null){
+            if(insert.Next != null && n < insert.Next.Value)
+            {
+                myLList.AddAfter(insert, n);
+                return insert.Next;
+            }
+            insert = insert.Next;
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
+    }
+}
