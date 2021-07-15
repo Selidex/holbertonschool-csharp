b9c3faf801c7e5c72df14f2b8e3aaa0366e3c9a9
Selidex <justindparnell@yahoo.com>
2021-07-14 20:20:16 -0500

    Uploading task 12 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary{
+    public static void PrintSorted(Dictionary<string, string> myDict){
+        List<string> newList = new List<string>();
+        foreach(KeyValuePair<string, string> entry in myDict)
+        {
+            newList.Add(entry.Key);
+        }
+        newList.Sort();
+        for(int i = 0; i < newList.Count; i++)
+            {
+                Console.WriteLine("{0}: {1}", newList[i], myDict[newList[i]]);
+            }
+    }
+}
