ce4895e190d7db0a4dafa055ba449398a7f2c999
Selidex <justindparnell@yahoo.com>
2021-07-15 14:17:47 -0500

    Uploading task 4 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search){
+        System.Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if(aQueue.Count == 0)
+            System.Console.WriteLine("Queue is empty");
+        else
+            System.Console.WriteLine("First item: {0}", aQueue.Peek());
+        aQueue.Enqueue(newItem);
+        System.Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        if(aQueue.Contains(search))
+        {
+            var k = "";
+            while(k != search)
+                k = aQueue.Dequeue();
+        }
+        return aQueue;
+    }
+}
