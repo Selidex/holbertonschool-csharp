d8e0f6153c8849a28bb837b8883bd8bd33e4426f
Selidex <justindparnell@yahoo.com>
2021-07-14 21:22:53 -0500

    Uploading task 15 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Matrix{
+    public static int[,] Square(int[,] myMatrix){
+        int x = myMatrix.GetLength(0);
+        int y = myMatrix.GetLength(1);
+        var newArray = new int [x,y];
+        for(int i = 0; i < x; i++)
+        {
+            for(int j = 0; j < y; j++)
+            {
+                newArray[i,j] = myMatrix[i,j] * myMatrix[i,j];
+            }
+        }
+        return newArray;
+    }
+}
