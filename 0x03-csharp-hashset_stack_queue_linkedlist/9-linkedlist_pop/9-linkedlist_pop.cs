d5827e5f93d431691f56b7ed1f72aa7dadb6d5c5
Selidex <justindparnell@yahoo.com>
2021-07-15 15:17:26 -0500

    Uploading task 9 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static int Pop(LinkedList<int> myLList){
+        if(myLList.First == null)
+            return 0;
+        int x = myLList.First.Value;
+        myLList.RemoveFirst();
+        return x;
+    }
+}
