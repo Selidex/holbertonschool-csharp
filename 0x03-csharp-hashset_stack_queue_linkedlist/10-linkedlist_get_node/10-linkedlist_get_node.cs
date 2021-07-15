cbd507bf619f165bf0a36452faba62b54e957274
Selidex <justindparnell@yahoo.com>
2021-07-15 15:22:15 -0500

    Uploading task 10 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList{
+    public static int GetNode(LinkedList<int> myLList, int n){
+        if(n < 0 || n >= myLList.Count)
+            return 0;
+        LinkedListNode<int> check = myLList.First;
+        for(int i = 0; i < n; i++)
+            check = check.Next;
+        return check.Value;
+    }
+}
