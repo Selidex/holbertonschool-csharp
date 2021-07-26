8181c0cfd1dcdefcb4dcc5aadc5b124fa727652c
Selidex <justindparnell@yahoo.com>
2021-07-26 14:28:31 -0500

    Uploading task 1 files'

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public enum Rating{
+    Good,
+    Great,
+    Excellent
+    }
+public struct Dog{
+    public  Rating rating;
+    public string name;
+    public string owner;
+    public float age;
+
+}
