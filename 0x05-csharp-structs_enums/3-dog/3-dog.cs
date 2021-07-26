494f4e313f69d34c68637b5f61603fbddb16d2af
Selidex <justindparnell@yahoo.com>
2021-07-26 14:41:13 -0500

    Uploading task 3 files

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public enum Rating{
+    Good,
+    Great,
+    Excellent
+    }
+public struct Dog{
+    public  Rating rating;
+    public string name;
+    public string owner;
+    public float age;
+    public Dog(string name, float age, string owner, Rating rating){
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public override string ToString(){
+        return $"Dog Name: {this.name}\nAge: {this.age}\nOwner: {this.owner}\nRating: {this.rating}";
+    }
+
+}
