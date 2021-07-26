0adc8a133429685b88dc32d425bd2a5fa57202f3
Selidex <justindparnell@yahoo.com>
2021-07-26 02:39:18 -0500

    Uploading task 1 files

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Int{
+    public static void divide(int a, int b){
+        try{
+            int x = a / b;
+            System.Console.WriteLine("{0} / {1} = {2}", a, b, a/b);
+        }
+        catch(DivideByZeroException){
+            System.Console.WriteLine("Cannot divide by zero");
+            System.Console.WriteLine("{0} / 0 = 0", a);
+        }
+    }
+}
