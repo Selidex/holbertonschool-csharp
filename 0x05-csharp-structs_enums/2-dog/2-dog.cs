2dc85360725d6599141026ce669f05feb757c087
Selidex <justindparnell@yahoo.com>
2021-07-26 14:32:35 -0500

    Uploading task 2 files

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public enum Rating{
+    Good,
+    Great,
+    Excellent
+    }
+public struct Dog{
+    public  Rating rating;
+    public string name;
+    public string owner;
+    public float age;
+    public Dog(string name, float age, string owner, Rating rating){
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+}
