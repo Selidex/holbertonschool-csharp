73736a6bed5d320bc0b01a562c7bf2fc6448a418
Selidex <justindparnell@yahoo.com>
2021-07-26 13:38:49 -0500

    Updated logic flow"

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -2,13 +2,16 @@
 
 class Int{
     public static void divide(int a, int b){
+        int x = 0;
         try{
-            int x = a / b;
-            System.Console.WriteLine("{0} / {1} = {2}", a, b, a/b);
+            x = a / b;
         }
         catch(DivideByZeroException){
             System.Console.WriteLine("Cannot divide by zero");
-            System.Console.WriteLine("{0} / 0 = 0", a);
+            x = 0;
+        }
+        finally{
+            System.Console.WriteLine("{0} / {1} = {2}", a, b, x);
+        }
         }
     }
-}
