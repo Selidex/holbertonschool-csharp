c302cc91e3142ce8ec06026566e9bf3f6e333565
Selidex <justindparnell@yahoo.com>
2021-07-27 19:17:37 -0500

    Uploading task 4 files

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+        public string Name{
+            get => name;
+            set => name = value;
+        }
+        public Zombie(){
+            this.health = 0;
+        }
+        public Zombie(int value){
+            if(value < 0){
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+        public int GetHealth(){
+            return this.health;
+        }
+    }
+}
