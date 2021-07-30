95d2390f8b127074edd4be7bffcdc19d13451607
Selidex <justindparnell@yahoo.com>
2021-07-29 21:52:45 -0500

    Added new tests

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -10,6 +10,9 @@ namespace Text.Tests
         {
             Assert.AreEqual(Str.UniqueChar("nnn"), -1);
             Assert.AreEqual(Str.UniqueChar("aabcc"), 2);
+            Assert.AreEqual(Str.UniqueChar("abbcc"), 0);
+            Assert.AreEqual(Str.UniqueChar(""), -1);
+            Assert.AreEqual(Str.UniqueChar("aabbc"), 4);
         }
     }
 }
