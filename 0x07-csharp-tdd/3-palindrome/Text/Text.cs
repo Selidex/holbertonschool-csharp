c2caf68f03d3b0938b4d5bd9278ff56773a282aa
Selidex <justindparnell@yahoo.com>
2021-07-29 21:14:23 -0500

    Fixed infinite loop

--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -19,6 +19,8 @@ namespace Text
                     return true;
                 if(s[i] != s[k])
                     return false;
+                i++;
+                k--;
             }
             return true;
 
