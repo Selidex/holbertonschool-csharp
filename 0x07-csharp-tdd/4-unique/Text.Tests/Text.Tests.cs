9fd6ea8e1ab3033daab1f283935d89919bfbcc41
Selidex <justindparnell@yahoo.com>
2021-07-29 21:49:14 -0500

    Updating logic

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -9,7 +9,7 @@ namespace Text.Tests
         public void Test1()
         {
             Assert.AreEqual(Str.UniqueChar("nnn"), -1);
-            Assert.AreEqual(Str.UniqueChar("aabcc"), -1);
+            Assert.AreEqual(Str.UniqueChar("aabcc"), 2);
         }
     }
 }
--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -5,17 +5,17 @@ namespace Text
     public class Str
     {
         public static int UniqueChar(string s){
-            int fb = 0;
-            int k = 0;
+            int k, count;
             for(int i = 0; i < s.Length; i++){
+                count = 0;
                 k = 0;
-                fb = 1;
-                while(k < s.Length){
+                while(k < s.Length)
+                {
                     if(s[i] == s[k])
-                        fb = 0;
+                        count++;
                     k++;
                 }
-                if(fb == 1)
+                if(count == 1)
                     return i;
             }
             return -1;
