d9d4579b0609f4165637741346f9e07db85d2aa5
Selidex <justindparnell@yahoo.com>
2021-08-02 06:05:51 -0500

    Uploading task 3 files

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Obj{
+    public static void Print(object myObj){
+        PropertyInfo[] myPropertyInfo;
+        MethodInfo[] myMethods;
+
+        myPropertyInfo = myObj.GetType().GetProperties();
+        myMethods = myObj.GetType().GetMethods();
+        System.Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        for (int i = 0; i < myPropertyInfo.Length; i++)
+        {
+            System.Console.WriteLine(myPropertyInfo[i].Name);
+        }
+        System.Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        for(int i = 0; i < myMethods.Length; i++)
+        {
+            System.Console.WriteLine(myMethods[i].Name);
+        }
+        
+    }
+}
