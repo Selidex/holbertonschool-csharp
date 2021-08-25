85dc7c36d04ae4960b97832898624333cd836223
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 12:22:09 -0500

    uploading the stuff

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath {
+    public static double Magnitude(double[] vector){
+            double k = 0;
+            
+            if (vector.Length != 2 && vector.Length != 3)
+                return -1;
+            for (int i = 0; i < vector.Length; i++){
+                k += Math.Pow(vector[i], 2);
+            }
+            return Math.Round(Math.Sqrt(k), 2);
+        }
+
+}
