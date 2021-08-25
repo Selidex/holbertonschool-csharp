bba935a4de94dc569c8b1eb071fd83d2d2e3bca1
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 14:01:00 -0500

    Uploading task 6

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath{
+    public static double[] Add(double[] vector1, double[] vector2){
+        if (vector1.Length != vector2.Length || (vector1.Length != 2 && vector1.Length != 3) || (vector2.Length != 2 && vector2.Length != 3) ){
+            double[] ret = new double[1] {-1};
+            return ret;
+        }
+        var list = new List<double>();
+        for (int i = 0; i < vector2.Length; i++){
+            list.Add(vector1[i] + vector2[i]);
+        }
+        var array = list.ToArray();
+        return array;
+    }
+}
