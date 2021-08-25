cb7f8a38b750fccf7ca77f39b398102ec4f22a9c
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 14:37:22 -0500

    uploading task 9 files

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath{
+    public static double[] Multiply(double[] vector, double scalar){
+        if (vector.Length != 2 && vector.Length != 3){
+            double[] ret = {-1};
+            return ret;
+        }
+        var list = new List<double>();
+        for(int i = 0; i < vector.Length; i++){
+            list.Add(vector[i] * scalar);
+        }
+        var array = list.ToArray();
+        return array;
+    }
+}
