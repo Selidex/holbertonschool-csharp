5fdac09af83f2e5c93f7d831ceede173e27e6050
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 22:07:25 -0500

    uploading task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 
 class MatrixMath{
     public static double[,] Add(double[,] matrix1, double[,] matrix2){
-        double[,] y = { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
-        if ((matrix1.Length != matrix2.Length) || (matrix1.Length != 2 && matrix1.Length != 3) || (matrix2.Length != 2 && matrix2.Length != 3) ||
-        (matrix1.Length != matrix1.GetLength(0) || matrix2.Length != matrix2.GetLength(0)) )
-            return y;
+        if ((matrix1.Length != matrix2.Length) || (matrix1.Length != 2 && matrix1.Length != 3))
+            return new double[,] {{-1}};
         for(int i = 0; i < matrix1.Length; i++){
             for(int j = 0; j < matrix1.Length; j++){
                 matrix1[i, j] += matrix2[i, j];
