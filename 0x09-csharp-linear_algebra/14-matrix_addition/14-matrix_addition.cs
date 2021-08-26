e53b5ca73c22e2d4d4d004528f9fba0cb706b4f7
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 21:01:07 -0500

    Uploading task 14 fixes

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,26 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class VectorMath{
+class MatrixMath{
     public static double[,] Add(double[,] matrix1, double[,] matrix2){
-        if (matrix1.Length != matrix2.Length)
-            return -1;
-        if (matrix1.Length != 2 && matrix1.Length != 3)
-            return -1;
-        if (matrix2.Length != 2 && matrix2.Length != 3)
-            return -1;
-        if(matrix1.Length != matrix1[i].Length || matrix2.Length != matrix2[i].Length)
-            return -1;
-
-        int x = matrix1.Length;
-        if (x == 2)
-            double[,] ret = new double[2,2];
-        if (x == 3)
-            double[,,] ret = new double[3,3,3];
-        for(int i = 0; i < x; i++){
-            for(int j = 0; j < x; j++)
-                ret[i][j] = matrix1[i][j] + matrix2[i][j];
+        double[,] y = { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
+        if ((matrix1.Length != matrix2.Length) || (matrix1.Length != 2 && matrix1.Length != 3) || (matrix2.Length != 2 && matrix2.Length != 3) ||
+        (matrix1.Length != matrix1.GetLength(0) || matrix2.Length != matrix2.GetLength(0)) )
+            return y;
+        for(int i = 0; i < matrix1.Length; i++){
+            for(int j = 0; j < matrix1.Length; j++){
+                matrix1[i, j] += matrix2[i, j];
+            }
         }
-        return ret;
+        return matrix1;
     }
 }
