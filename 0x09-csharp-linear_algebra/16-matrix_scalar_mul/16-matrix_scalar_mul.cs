8255c212668991afd1f5f74827955cb85e594ca7
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 22:41:36 -0500

    upload task 16

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar){
+        int x0 = matrix.GetLength(0), x1 = matrix.GetLength(1);
+        if(x0 != x1 || (x0 != 2 && x0 != 3))
+            return new double[,] {{-1}};
+        
+        double[,] ret = new double[x0,x0];
+        for (int i = 0; i < x0; i++){
+            for(int j = 0; j < x1; j++)
+                ret[i, j] = matrix[i,j] * scalar;
+        }
+        return ret;
+    }
+}
