1a6937b80dde7dcc8f252b47e25e39360dcec545
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-26 12:56:47 -0500

    Fixed empty return case

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 class MatrixMath{
     public static double[,] Transpose(double[,] matrix){
         if (matrix == null || matrix.Length == 0)
-            return new double[,] {{}};
+            return new double[,]{};
         double[,] ret = new double[matrix.GetLength(1), matrix.GetLength(0)];
         for (int i = 0; i < ret.GetLength(0); i++){
             for (int j = 0; j < ret.GetLength(1); j++){
