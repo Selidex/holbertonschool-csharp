6d0f492e25655720bf96d45217d178febf09d587
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-26 00:36:44 -0500

    Uploading base framework

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MatrixMath{
+    public static double[,] Rotate2D(double[,] matrix, double angle){
+        if(matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
+        return matrix;
+    }
+}
