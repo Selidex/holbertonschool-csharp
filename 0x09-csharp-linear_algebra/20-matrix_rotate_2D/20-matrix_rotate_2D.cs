3e6dc6d1f8db2b478bd1d15e1b96fb91b9ecfcfc
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-26 14:03:06 -0500

    uploading task 20 files

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -5,6 +5,13 @@ class MatrixMath{
     public static double[,] Rotate2D(double[,] matrix, double angle){
         if(matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] {{-1}};
-        return matrix;
+        if (angle == 0)
+            return matrix;
+        double[,] ret = new double[2,2];
+        ret[0,0] = Math.Round((matrix[0,0] * Math.Cos(angle)) - (matrix[0,1] * Math.Sin(angle)));
+        ret[0,1] = Math.Round((matrix[0,0] * Math.Sin(angle)) + (matrix[0,1] * Math.Cos(angle)));
+        ret[1,0] = Math.Round((matrix[1,0] * Math.Cos(angle)) - (matrix[1,1] * Math.Sin(angle)));
+        ret[1,1] = Math.Round((matrix[1,0] * Math.Sin(angle)) + (matrix[1,1] * Math.Cos(angle)));
+        return ret;
     }
 }
