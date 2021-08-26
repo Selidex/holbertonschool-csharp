c731ab1138b7627311a71e6078a789fe29c9a7e5
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-26 12:02:15 -0500

    Uploading task 30 files

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath{
+    public static double[] CrossProduct(double[] vector1, double[] vector2){
+        if(vector1.Length != 3 || vector2.Length != 3)
+            return new double[] {-1};
+        double a = vector1[0], b = vector1[1], c = vector1[2], d = vector2[0], e = vector2[1], f = vector2[2];
+        double[] ret = new double[3];
+        ret[0] = (b * f) - (c * e);
+        ret[1] = (c * d) - (a * f);
+        ret[2] = (a * e) - (b * d);
+
+        return ret;
+    }
+}
