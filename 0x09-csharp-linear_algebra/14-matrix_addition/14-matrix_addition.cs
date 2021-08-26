0ad86b96f9d81e3480d6d063417be7a868a57f85
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-25 22:34:12 -0500

    Updated loop end points

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 
 class MatrixMath{
     public static double[,] Add(double[,] matrix1, double[,] matrix2){
-        if ((matrix1.Length != matrix2.Length) || (matrix1.Length != 2 && matrix1.Length != 3))
-            return new double[,] {{-1}};
-        for(int i = 0; i < matrix1.Length; i++){
-            for(int j = 0; j < matrix1.Length; j++){
-                matrix1[i, j] += matrix2[i, j];
+        int x0 = matrix1.GetLength(0), x1 = matrix1.GetLength(1), y0 = matrix2.GetLength(0), y1 = matrix2.GetLength(1);
+        if (x0 != x1 || y0 != y1 || x0 != y0 || (x0 != 2 && x0 !=3) || (y0 != 2 && y0 != 3))
+            return new double[,]{{-1}};
+        double[,] ret = new double[x0,x0]; 
+        for(int i = 0; i < matrix1.GetLength(0); i++){
+            for(int j = 0; j < matrix1.GetLength(1); j++){
+                ret[i,j] = matrix2[i, j] + matrix1[i, j];
             }
         }
         return matrix1;
