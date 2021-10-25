dbe3af63f3c1d141fc454f6f58ba8992b77080ce
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-10-25 14:24:42 -0500

    Uploading task 1

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+    class Queue<T>
+    {
+        public object CheckType(){
+            return typeof(T);
+        }
+        class Node{
+            public dynamic value = null;
+            public Node next = null;
+            public Node(dynamic val){
+                value = val;
+            }
+        }
+        Node head;
+        Node tail;
+        int count;
+        public void Enqueue(dynamic value){
+            Node king = new Node(value);
+            if(count == 0){
+                head = king;
+                tail = king;
+            }
+            else{
+                tail.next = king;
+                tail = king;}
+            count++;
+            }
+        public int Count(){
+            return count;
+        }
+    }
