d2f6c4139990379608e5c6f790b7755fa03ba7a6
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-10-26 18:07:14 -0500

    Uploading task 6

---
+++ 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Base{
+    public string name {get; set;}
+    public override string ToString(){
+        return name + " is a " + this.GetType().Name;
+    }
+}
+
+interface IInteractive{
+    void Interact();
+}
+
+interface IBreakable{
+    int durability {get; set;}
+    void Break();
+}
+
+interface ICollectable{
+    bool isCollected{get; set;}
+    void Collect();
+}
+
+class Door : Base, IInteractive{
+    public Door(string val="Door"){
+        name = val;
+    }
+    public void Interact(){
+        System.Console.WriteLine("You try to open the {0}. It's locked.", this.name);
+    }
+}
+
+class Decoration : Base, IInteractive, IBreakable{
+    public bool isQuestItem;
+    public int durability{get; set;}
+    public Decoration(string name="Decoration", int durability=1, bool isQuestItem=false){
+        this.name = name;
+        if(durability < 1){
+            throw new ArgumentException("Durability must be greater than 0");
+        }
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+    public void Interact(){
+        if(durability < 1){
+            System.Console.WriteLine("The {0} has been broken.", this.name);
+        }
+        else if(isQuestItem == true){
+            System.Console.WriteLine("You look at the {0}. There's a key inside.", this.name);
+        }
+        else{
+            System.Console.WriteLine("You look at the {0}. Not much to see here.", this.name);
+        }
+    }
+
+    public void Break(){
+        durability--;
+        if(durability > 0){
+            System.Console.WriteLine("You hit the {0}. It cracks.", this.name);
+        }
+        else if(durability == 0){
+            System.Console.WriteLine("You smash the {0}. What a mess.",this.name);
+        }
+        else{
+             System.Console.WriteLine("The {0} is already broken.",this.name);
+        }
+    }
+}
+
+class Key : Base, ICollectable{
+    public bool isCollected{get; set;}
+    public Key(string name="Key", bool isCollected=false){
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+    public void Collect(){
+        if(this.isCollected == false){
+            this.isCollected = true;
+            System.Console.WriteLine("You pick up the {0}.", this.name);
+        }
+        else{
+            System.Console.WriteLine("You have already picked up the {0}.", this.name);
+        }
+    }
+}
+
+class Objs<T> : IEnumerable<T>{
+     public List<T> MyList {get; set;}
+
+    public Objs() { MyList = new List<T>(); }
+    public void Add(T thing){
+        MyList.Add(thing);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return MyList.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
