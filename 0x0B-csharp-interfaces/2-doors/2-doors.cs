d077c8b78f8c26d747f01db75414ac1b3b443d32
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-10-26 16:41:15 -0500

    Uploading task 2

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Base{
+    public string name {get; set;}
+    public override string ToString(){
+        return name + " is a " + this.GetType().Name;
+    }
+}
+
+interface IInteractive{
+    void Interact();
+}
+
+interface IBreakable{
+    int durability {get; set;}
+    void Break();
+}
+
+interface ICollectable{
+    bool isCollected{get; set;}
+    void Collect();
+}
+
+class Door : Base, IInteractive{
+    public Door(string val="Door"){
+        name = val;
+    }
+    public void Interact(){
+        System.Console.WriteLine("You try to open the {0}. It's locked.", this.name);
+    }
+}
