3e9dda9f80a53f8326813b7a2ecc332aca979b17
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-10-26 16:04:45 -0500

    Uploading task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Base{
+    public string name = null;
+    public override string ToString(){
+        return name + " is a " + this.GetType().Name;
+    }
+}
+
+interface IInteractive{
+    void Interact();
+}
+
+interface IBreakable{
+    int durability {get; set;}
+    void Break();
+}
+
+interface ICollectable{
+    bool isCollected{get; set;}
+    void Collect();
+}
+
+class TestObject : Base, ICollectable, IBreakable, IInteractive{
+    public void Collect(){}
+    public void Break(){}
+    public void Interact(){}
+    public bool isCollected{ get; set;}
+    public int durability {get; set;}
+}
+
