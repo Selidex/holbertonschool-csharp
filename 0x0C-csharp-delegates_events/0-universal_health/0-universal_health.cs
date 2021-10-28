29cda40aaaafac31f87e5b4aafe0017620aa021b
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-10-28 10:43:15 -0500

    Uploading task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Player{
+    private string name;
+    private float maxHp;
+    private float hp;
+    public Player(string name, float maxHp=100f){
+        this.name = name;
+        if(maxHp <= 0){
+            maxHp = 100f;
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    public void PrintHealth(){
+        System.Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+}
